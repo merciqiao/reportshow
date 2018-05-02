3d1dc1fa0cab4e461791f6fa4b7129cf5ec96da6
merciqiao <qiaolixue@yingu.com>
2018-05-02 10:16:51 +0800

    1.接口url改成读取配置

---
+++ js/config.js
@@ -0,0 +1,12 @@
+//开发环境:dev,测试环境:test,生产环境:product
+var evn='test';
+
+var Global={
+    dev:{
+        report_api_url:'http://localhost:8804/'
+    },
+    test:{
+        report_api_url:'http://172.24.132.134:8803/'
+    }
+    
+};
