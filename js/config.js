f440e79e4cff8c391e220a1bd0f757348a6c95ce
merciqiao <qiaolixue@yingu.com>
2018-05-02 13:30:57 +0800

    1.添加本地配置

--- js/config.js
+++ js/config.js
@@ -1,9 +1,12 @@
-//开发环境:dev,测试环境:test,生产环境:product
-var evn='test';
+//本机环境:localhost,开发环境:dev,测试环境:test,生产环境:product
+var evn='dev';
 
 var Global={
-    dev:{
+    localhost:{
         report_api_url:'http://localhost:8804/'
+    },
+    dev:{
+        report_api_url:'http://172.24.132.134:8804/'
     },
     test:{
         report_api_url:'http://172.24.132.134:8803/'
